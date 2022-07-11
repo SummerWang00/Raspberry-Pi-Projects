1948fcc21a2b4bcb98efb62e82023b1debb2f7c7
SummerWang00 <summerw1015@gmail.com>
2022-07-11 10:43:50 -0400

    Added json practice file

---
+++ RFID_Project/raspberrypi_examplecode/python/json practice/lesson.py
@@ -0,0 +1,23 @@
+{
+    "names": [
+        {
+            "firstname": "Jim",
+            "age": 32
+        },
+        {
+            "firstname": "Fred",
+            "age": 32
+        },
+        {
+            "firstname": "Bob",
+            "age": "28"
+        },
+        {
+            "firstname": "Joe",
+            "age": 40
+        }
+    ]
+}
+{
+    "names"
+}
