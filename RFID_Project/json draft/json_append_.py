c64fbdb6128e3a7b0343cb12d25ffe71f64c2c7c
SummerWang00 <summerw1015@gmail.com>
2022-07-21 14:48:31 -0400

    Improved code readability

--- RFID_Project/json draft/json_append_.py
+++ RFID_Project/json draft/json_append_.py
@@ -16,3 +16,19 @@ with open(filename) as json_file:
 #   name = input("Please Enter")
 
 write_json(data)
+
+import json
+
+filename = "name.json"
+
+
+# open file, and dump file content into data.
+def write_json(data: object, filename: str = filename):
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+with open(filename) as json_file:
+    data = json.load(json_file) # opsn the json fil
+    y = {"firstname": "Joseph", "age": 29, "ID#": 1823908214214}  # what's being added
+    data["names"].append(y)   
