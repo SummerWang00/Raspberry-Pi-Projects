03c86818cf84fdedd92f9ba13b101c66b3d162dd
SummerWang00 <summerw1015@gmail.com>
2022-08-02 09:31:46 -0400

    Fixes on function logic in database program main while loop

--- RFID_Project/My project/scanner_get_id.py
+++ RFID_Project/My project/scanner_get_id.py
@@ -19,7 +19,44 @@ file = "name_ID_data.json"
 
 # global variables
 secs = 10
-access = False
+
+def check_access(int_uid: int):
+    access = False
+    with open (file, encoding="utf-8",) as json_file: #encoding="utf-8"
+        data = json.load(json_file)
+        name_data = (data["names"])       #make name_data into a list of individual dictionary\
+
+        for i in name_data:      # access True?
+            name = (i["name"])   # adding end="" get rid of the audo new line character
+            ID = (i["ID#"])
+            if int_uid == ID:
+                # Green LED light up
+                access = True
+                break
+            else:
+                continue
+
+        if access == True:
+            print(f"This is {name}'s card, access granted, door opened", end="\n\n")
+        else:
+            print("I don't know you, access denied!\n")  # when this line is indented it turns grey
+
+    access = False
+    time.sleep(1)
+    '''
+    logic:
+    Compare UID to existing database
+    access = False
+    for ID in <database ID data>:   #check through every single name and ID pair to find matches
+        if ID == int_id:
+            print("Access allowed for <name>")
+        else:
+            print("Access denied")
+            access = false
+        if access = true:
+            access = false
+    '''
+    return access
 
 if __name__ == '__main__':
     try:
@@ -33,7 +70,6 @@ if __name__ == '__main__':
         #pn532 = PN532_UART(debug=False, reset=20)
 
         # SET UP
-
         ic, ver, rev, support = pn532.get_firmware_version()
         print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
 
@@ -41,17 +77,15 @@ if __name__ == '__main__':
         pn532.SAM_configuration()
 
         #Start of program, choose mode
-        input('Enter "r" to for ID reader mode and "e" to add or remove a person from database: ')
+        #input('Enter "r" to for ID reader mode and "e" to add or remove a person from database: ')
         print('Waiting for RFID/NFC card...')
         ID_undetected_time: int = 0
         while True:
             # Check if at ID_undetected_time card is available to read
             # read_passive_target get the uid and store it in variable uid.
-
             # The message doesn't print after so long idk why???
             # It's the print statemenet's fault. Buffer doesn't flush immediately
-            # I want to print "Still waiting" after 1o timeouts. Every  loop
-
+            # I want to print "Still waiting" after 1o timeouts. Every loop
             if ID_undetected_time == secs:
                 ID_undetected_time = 0
                 print("Where's the card? Still waiting~")
@@ -75,8 +109,8 @@ if __name__ == '__main__':
             ID_undetected_time = 0
             # print('Found card with UID:', [uid.decode()])   #prints the same as hex(i)
             # print(type((uid).decode()))  #this returned class 'bytes', so it is ID_undetected_time byte object? Using the function byte() can turn ID_undetected_time list/number into ID_undetected_time byte object
-                                            #this is very weird, error message 'utf-8' codec can't decode byte 0xb1 in position 2: invalid start byte
-            #imagine I open the json database file here
+                                           #this is very weird, error message 'utf-8' codec can't decode byte 0xb1 in position 2: invalid start byte
+            #open the json database file here to check for access. Ended up using a function for access
 
             # right after acquiring card ID, check if card holder has access or not.
             check_access(int_uid)
@@ -86,38 +120,3 @@ if __name__ == '__main__':
 
     finally:
         GPIO.cleanup()
-
-def check_access(int_uid):
-            with open (file, encoding="utf-8",) as json_file: #encoding="utf-8"
-                data = json.load(json_file)
-                name_data = (data["names"])       #make name_data into a list of individual dictionary\
-
-                for i in name_data:
-                    name = (i["name"])   # adding end="" get rid of the audo new line character
-                    ID = (i["ID#"])
-                    if int_uid == ID:
-                        # Green LED light up
-                        access = True
-                        print(f"This is {name}'s card, access granted, door opened", end="\n\n")
-                        break
-                    else:
-                        continue
-
-                if access == False:
-                    print("I don't know you, access denied!")  # when this line is indented it turns grey
-
-            access = False
-            time.sleep(1)
-            '''
-            logic:
-            Compare UID to existing database
-            access = False
-            for ID in <database ID data>:   #check through every single name and ID pair to find matches
-                if ID == int_id:
-                    print("Access allowed for <name>")
-                else:
-                    print("Access denied")
-                    access = false
-                if access = true:
-                    access = false
-            '''
